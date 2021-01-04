9da2ce0ef6d4997abc41b18a9333650d69867e14
Colton Voege <colton@mimirhq.com>
2021-01-04 17:04:41 -0500

    prettier-ruby issue example

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+// @flow
+
+// .prettierrc.js
+module.exports = {
+  printWidth: 100,
+  arrowParens: "always",
+  trailingComma: "es5",
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  requirePragma: false,
+  proseWrap: "preserve",
+
+  // prettier-ruby
+  rubyArrayLiteral: false,
+  rubyHashLabel: true,
+  rubyModifier: true,
+  rubySingleQuote: true,
+  rubyToProc: true,
+};
